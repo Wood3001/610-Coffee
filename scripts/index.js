a3c21fcf2e3b8d48449da8f8bb550f34f0eced96
Wood3001 <wood3001@gmail.com>
2022-12-13 18:09:57 -0800

    As Submitted for MDIA 3293 Final Project

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+//////// SLICK SLIDER /////////
+
+$(document).ready(function(){
+    $('.slides').slick({
+        arrows: false,
+        adaptiveHeight: true,
+        autoplay: true,
+        vertical: true,
+    });
+  });
